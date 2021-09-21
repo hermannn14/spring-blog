1c95d7a04b872b76a58b592415e63c9d94842b27
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-09-21 10:32:51 -0500

    Need to reboot machine

---
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -0,0 +1,22 @@
+//package com.codeup.springblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HelloController {
+//
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String helloFromSpring() {
+//        return "Hello from the world of Spring Boot";
+//    }
+//
+//    @GetMapping("/helloWorld/{username}")
+//    @ResponseBody
+//    public String helloWorld(@PathVariable String username) {
+//        return "Hello World";
+//    }
+//}
