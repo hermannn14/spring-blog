885ece47144d0eeef179c907d2ecda4e2aeafba8
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-09-25 22:48:06 -0500

    Passing data to view exercise progress

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
@@ -11,11 +12,13 @@ import java.util.Random;
 public class RollDiceController {
 
     @GetMapping("/roll-dice")
+    @ResponseBody
     public String rollDice() {
         return "roll-dice";
     }
 
     @GetMapping("/roll-dice/{n}")
+    @ResponseBody
     public String rollDiceGuess(@PathVariable int n, Model model) {
         Random rand = new Random();
         int random = rand.nextInt((6-1) + 1) +1;
