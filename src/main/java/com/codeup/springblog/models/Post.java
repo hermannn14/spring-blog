8283b6bf66faaa645f68402f08fea8ec9b22e0e3
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-10-05 20:15:36 -0500

    Working on delete ad function

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -8,7 +8,7 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String title;
